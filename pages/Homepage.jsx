c4023e6a2fea82e3f7716314660080b4a783296c
SimoneCriscuolo233 <simone.criscuolo26052001@gmail.com>
2025-10-06 18:01:58 +0200

    fix  homepage

--- pages/Homepage.jsx
+++ pages/Homepage.jsx
@@ -20,32 +20,32 @@ const Homepage = () => {
     <div>
       <div className="container mt-5">
         <div className="row">
+          <div className="col-12">
+            <h1>Prodotti in evidenza</h1>
+          </div>
+          <div className="col-6">
+            <img
+              src="../public/jumbotron/herospace.png"
+              alt="abiti in evidenza"
+            />
+          </div>
+
           {products.map((product) => {
             return (
-              <>
-                <div className="col-12" key={product.id}>
-                  <h1>Prodotti in evidenza</h1>
-                </div>
-                <div className="col-6">
+              <div className="col-6" key={product.id}>
+                <div className="card" style={{ width: "18 rem" }}>
                   <img
-                    src="../public/jumbotron/herospace.png"
-                    alt="abiti in evidenza"
+                    src={product.image}
+                    className="card-img-top"
+                    alt={product.name}
                   />
-                </div>
-                <div className="col-6">
-                  <div className="card" style={{ width: "18 rem" }}>
-                    <img
-                      src={product.image}
-                      className="card-img-top"
-                      alt={product.name}
-                    />
-                    <div className="card-body">
-                      <h5 className="card-title">{product.name}</h5>
-                      <p className="card-text">{product.price}</p>
-                    </div>
+                  <div className="card-body">
+                    <h5 className="card-title">{product.name}</h5>
+                    <p className="card-text">{product.price}</p>
                   </div>
                 </div>
-              </>
+              </div>
+
             );
           })}
         </div>
