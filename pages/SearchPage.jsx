cc0c0502f72f96f8919a3c4161777a7e443f3280
SimoneCriscuolo233 <simone.criscuolo26052001@gmail.com>
2025-10-09 11:38:25 +0200

    fix: remove unnecessary column class from product display in SearchPage

--- pages/SearchPage.jsx
+++ pages/SearchPage.jsx
@@ -146,7 +146,7 @@ const SearchPage = () => {
           ) : (
             <div className="row">
               {filtered.map((product) => (
-                <div key={product.id} className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+                <div key={product.id} className="col-12 col-md-4 col-lg-3 mb-4">
                   <ProductCard product={product} />
                 </div>
               ))}
