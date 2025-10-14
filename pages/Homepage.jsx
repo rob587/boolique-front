47604e81e0db998d17be326d2ef56072fa39c260
MelaniaPicone <melania_picone@yahoo.it>
2025-10-14 11:12:15 +0200

    [fix] responsive

--- pages/Homepage.jsx
+++ pages/Homepage.jsx
@@ -89,7 +89,6 @@ const Homepage = () => {
         </div>
       </div>
 
-        <div className="col-12 text-center my-5 imglogospace"><img className="logospace" src="/imgs/logo.png" alt="logo" /></div>
 
       <div className="col-12 text-center">
         <h1 className="title">BENVENUTO IN BOOLIQUE</h1>
